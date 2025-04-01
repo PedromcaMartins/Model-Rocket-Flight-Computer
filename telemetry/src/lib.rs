8f22d054f19c74eb34b265e4c35a35a0d1a4d6e6
PedromcaMartins <pedromcamartins@duck.com>
2025-04-01 12:50:40 +0100

    feat: enhance telemetry messages with serde and defmt support; update IMU and altimeter structures

--- telemetry/src/lib.rs
+++ telemetry/src/lib.rs
@@ -1,44 +1,62 @@
 #![no_std]
 
-use mint::{EulerAngles, Quaternion, Vector3};
 use chrono::{NaiveDate, NaiveTime};
 use nmea::sentences::FixType;
 
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+#[derive(Debug, Clone, Copy)]
 pub struct ImuMessage {
-    pub euler_angles: EulerAngles<f32, ()>,
-    pub quaternion: Quaternion<f32>,
-    pub linear_acceleration: Vector3<f32>,
-    pub gravity: Vector3<f32>,
-    pub acceleration: Vector3<f32>,
-    pub gyro: Vector3<f32>,
-    pub mag: Vector3<f32>,
+    /// Euler angles representation of heading in degrees.
+    /// Euler angles is represented as (`roll`, `pitch`, `yaw/heading`).
+    pub euler_angles: [f32; 3],
+    /// Standard quaternion represented by the scalar and vector parts. Corresponds to a right-handed rotation matrix.
+    /// Quaternion is represented as (x, y, z, s).
+    ///
+    /// where:
+    /// x, y, z: Vector part of a quaternion;
+    /// s: Scalar part of a quaternion.
+    pub quaternion: [f32; 4],
+    /// Linear acceleration vector in m/s^2 units.
+    pub linear_acceleration: [f32; 3],
+    /// Gravity vector in m/s^2 units.
+    pub gravity: [f32; 3],
+    /// Acceleration vector in m/s^2 units.
+    pub acceleration: [f32; 3],
+    /// Gyroscope vector in deg/s units.
+    pub gyro: [f32; 3],
+    /// Magnetometer vector in uT units.
+    pub mag: [f32; 3],
+    /// Temperature of the chip in Celsius degrees.
     pub temperature: f32,
 }
 
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+#[derive(Debug, Clone, Copy)]
 pub struct AltimeterMessage {
+    /// Pressure in Pascal.
     pub pressure: f64,
-    pub temperature: f64,
+    /// Altitude in meters.
+    pub altitude: f32,
+    /// Temperature in Celsius degrees.
+    pub temperature: f32,
 }
 
-pub enum GpsMessage {
-    GGA {
-        fix_time: NaiveTime,
-        fix_type: FixType,
-        latitude: f64,
-        longitude: f64,
-        /// MSL Altitude in meters
-        altitude: f32,
-        num_of_fix_satellites: u8,
-        hdop: f32,
-        geoid_separation: f32,
-    },
-    RMC {
-        fix_time: NaiveTime,
-        fix_date: NaiveDate,
-        fix_type: FixType,
-        latitude: f64,
-        longitude: f64,
-        speed_over_ground: f32,
-        course_over_ground: f32,
-    },
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[cfg_attr(feature = "defmt-03", derive(defmt::Format))]
+#[derive(Debug, Clone, Copy)]
+pub struct GpsMessage {
+    #[cfg_attr(feature = "defmt-03", defmt(Debug2Format))]
+    pub fix_time: NaiveTime,
+    #[cfg_attr(feature = "defmt-03", defmt(Debug2Format))]
+    pub fix_type: FixType,
+    /// Latitude in degrees.
+    pub latitude: f64,
+    /// Longitude in degrees.
+    pub longitude: f64,
+    /// MSL Altitude in meters
+    pub altitude: f32,
+    /// Number of satellites used for fix.
+    pub num_of_fix_satellites: u8,
 }
